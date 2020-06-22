cd220dea3d8e5e80eb6e519d39d20dfcf1a5f97c
Ellis Percival <flyte@failcode.co.uk>
2020-06-22 12:12:36 +0100

    Add first version

---
+++ popup.js
@@ -0,0 +1,26 @@
+function setButton() {
+    const button = document.getElementById("en_disable");
+    if (button !== null) {
+        chrome.storage.sync.get("disabled", function(result) {
+            let disabled;
+            if (result !== undefined && result.disabled) {
+                disabled = true;
+                button.innerText = "Enable";
+            } else {
+                disabled = false;
+                button.innerText = "Disable";
+            }
+            button.onclick = function() {
+                chrome.storage.sync.set({disabled: !disabled}, function() {
+                    const enDisabledText = (disabled) ? "enabled" : "disabled";
+                    console.log("Remember Django Version is now " + enDisabledText);
+                    setButton()
+                });
+            }
+        })
+    }
+}
+
+window.onload = function() {
+    setButton()
+}
